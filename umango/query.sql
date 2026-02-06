0c975dd2febddca73ca6b25cf745996587d31b90
Rafael <rafael.bzonta@gmail.com>
2026-02-06 12:58:37 -0300

    Update query.sql

--- umango/query.sql
+++ umango/query.sql
@@ -1,16 +1,21 @@
 sql
 
 INSERT INTO SCAN_PROCESSADO (
+    
     USUARIO_SCAN,
     ORDEM_SERVICO,
     SETOR,
     EMAIL_CLIENTE,
     VALOR_OCR_ZONA_2
+    
 )
+    
 VALUES (
+    
     '{{user}}',
     '{{ordem_servico}}',
     '{{setor}}',
     '{{email_cliente}}',
     '{{valor_ocr_zona_2}}'
+    
 );
